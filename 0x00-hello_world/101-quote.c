1451450c12f120f21ddd6e8cf1a823861cdc6e67
maskoura <maskouraaa5000@gmail.com>
2023-06-20 01:38:28 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-
 #include <unistd.h>
 
-
 /**
+ * main - Entry point
  *
- * * main - Entry point
- *
- * *
- *
- * * Return: Always 1 (Success)
- *
+ * Return: Always 1 (Success)
  */
 
 int main(void)
@@ -19,9 +13,7 @@ int main(void)
 
 	write(2,
 
-
 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
 
 	return (1);
 
